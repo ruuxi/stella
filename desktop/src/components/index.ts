dea5b9037a9f544a608335827f18db223090c3ef
ruuxi <lolruuxi@gmail.com>
2026-01-29 00:37:37 -0700

    wip onboarding

--- desktop/src/components/index.ts
+++ desktop/src/components/index.ts
@@ -74,6 +74,7 @@ export { Avatar, type AvatarProps } from "./avatar";
 // Content Display Components
 export { Code, type CodeProps } from "./code";
 export { Typewriter, type TypewriterProps } from "./typewriter";
+export { OnboardingStep1, useOnboardingState } from "./Onboarding";
 export { ImagePreview, type ImagePreviewProps } from "./image-preview";
 export { Tag, type TagProps } from "./tag";
 export { ProgressCircle, type ProgressCircleProps } from "./progress-circle";
