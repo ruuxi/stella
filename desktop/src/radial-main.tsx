aaa546c692f5d71d031a515d642a7eba1fd400b1
ruuxi <lolruuxi@gmail.com>
2026-02-02 21:54:32 -0700

    ixes to dial stuff

---
+++ desktop/src/radial-main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { ThemeProvider } from './theme/theme-context'
+import { RadialDial } from './screens/RadialDial'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ThemeProvider>
+      <div className="app window-radial">
+        <div className="radial-shell">
+          <RadialDial />
+        </div>
+      </div>
+    </ThemeProvider>
+  </StrictMode>,
+)
