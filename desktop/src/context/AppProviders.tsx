bd4e6ff6ed32d35fd8a99d53af334cb0717d2887
ruuxi <lolruuxi@gmail.com>
2026-03-14 20:51:49 -0700

    [game]: remove as global

--- desktop/src/context/AppProviders.tsx
+++ desktop/src/context/AppProviders.tsx
@@ -3,20 +3,17 @@ import { DevProjectsProvider } from './dev-projects-state'
 import { ThemeProvider } from './theme-context'
 import { UiStateProvider } from './ui-state'
 import { WorkspaceProvider } from './workspace-state'
-import { SpacetimeGamesProvider } from '@/features/games/SpacetimeGamesProvider'
 import { ToastProvider } from '@/ui/toast'
 
 export function AppProviders({ children }: { children: ReactNode }) {
   return (
     <ThemeProvider>
       <ToastProvider>
-        <SpacetimeGamesProvider>
-          <UiStateProvider>
-            <DevProjectsProvider>
-              <WorkspaceProvider>{children}</WorkspaceProvider>
-            </DevProjectsProvider>
-          </UiStateProvider>
-        </SpacetimeGamesProvider>
+        <UiStateProvider>
+          <DevProjectsProvider>
+            <WorkspaceProvider>{children}</WorkspaceProvider>
+          </DevProjectsProvider>
+        </UiStateProvider>
       </ToastProvider>
     </ThemeProvider>
   )
