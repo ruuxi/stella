9e2fb2703780306abc93c16cf8b8f40b7691fe2e
ruuxi <lolruuxi@gmail.com>
2026-03-21 17:30:55 -0700

    [desktop]: strict off

--- desktop/templates/workspace-game-app/src/main.tsx
+++ desktop/templates/workspace-game-app/src/main.tsx
@@ -1,9 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { AppRoot } from "./AppRoot";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <AppRoot />
-  </React.StrictMode>,
-);
+createRoot(document.getElementById("root")!).render(<AppRoot />);
